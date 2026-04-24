cc7f6092cf2d87793e662f0bb87f5bce16819ab4
Brilloku <briankuria2020@gmail.com>
2026-04-24 18:41:33 +0300

    space test

--- pesaki-server/src/api/routes/mpesa.ts
+++ pesaki-server/src/api/routes/mpesa.ts
@@ -95,6 +95,7 @@ export const mpesaRoutes = async (fastify: FastifyInstance) => {
     });
   });
 
+  
   // Confirmation URL
   fastify.post('/c', async (request: FastifyRequest, reply: FastifyReply) => {
     const body: any = request.body;
