3aeb09da723267258dbc14a856b404c17372651c
DebugOnline <onlinebusinesses190@gmail.com>
2026-06-07 01:03:47 +0300

    FINAL22233443322

--- pesaki/src/app/dashboard/fx/page.tsx
+++ pesaki/src/app/dashboard/fx/page.tsx
@@ -206,7 +206,7 @@ export default function FXPage() {
                 <div className="flex items-center justify-between px-2">
                     <div>
                         <h1 className="text-xl lg:text-3xl font-bold text-white flex items-center gap-2">
-                            <Activity className="text-primary w-5 h-5 lg:w-8 lg:h-8" /> </Binary> FX
+                            <Activity className="text-primary w-5 h-5 lg:w-8 lg:h-8" /> Binary FX
                         </h1>
                         <div className="flex items-center gap-2 mt-1">
                             <select
