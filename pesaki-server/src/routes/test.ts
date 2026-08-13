c4780f724d1b33e7be7f192a20fc2bb3fdcc5633
DebugOnline <onlinebusinesses190@gmail.com>
2026-08-13 05:35:26 +0300

    Delete pesaki-server/src/routes/test.ts

--- pesaki-server/src/routes/test.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance } from 'fastify';
-
-export const testRoutes = async (fastify: FastifyInstance) => {
-  fastify.get('/ping', async (request, reply) => {
-    reply.send({ status: 'ok' });
-  });
-};
